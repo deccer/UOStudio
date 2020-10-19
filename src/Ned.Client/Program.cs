c55e7260b72504a13f778b2d84323da9c780797a
deccer <decccer@outlook.com>
2020-10-19 09:17:15 +0400

    Add solution skeleton

---
+++ src/Ned.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ned.Client
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
