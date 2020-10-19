a1488e75dfc3dc41a9da61c4cdb602992b2e554a
deccer <decccer@outlook.com>
2020-10-19 09:31:37 +0400

    Add server project skeleton

---
+++ src/Ned.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ned.Server
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
