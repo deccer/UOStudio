c09f6c7fc7276b8a6f4b1b340af4a59d3cc167a4
deccer <decccer@outlook.com>
2020-10-24 14:59:29 +0400

    Cleanup files

--- src/UOStudio.Client.Engine/Ultima/UltimaConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UOStudio.Client.Engine.Ultima
-{
-    internal static class UltimaConstants
-    {
-        public const int MaxStaticDataIndexCount = 65536;
-        public const int MaxLandDataIndexCount = 16384;
-    }
-}
