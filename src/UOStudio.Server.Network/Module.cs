d101baaa429daa9fcb8e89016447fc808193a5c3
deccer <decccer@outlook.com>
2020-11-14 16:30:55 +0400

    Add network packet handling facility

---
+++ src/UOStudio.Server.Network/Module.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UOStudio.Server.Network.PacketHandlers;
+
+namespace UOStudio.Server.Network
+{
+    public static class Module
+    {
+        public static void AddNetworkServer(this IServiceCollection services)
+        {
+            services.AddSingleton<IPacketHandler<ClientConnectPacket, int>, ClientConnectPacketHandler>();
+            services.AddSingleton<IPacketProcessor, PacketProcessor>();
+            services.AddSingleton<IAccountStore, AccountStore>();
+            services.AddSingleton<INetworkServer, NetworkServer>();
+        }
+    }
+}
