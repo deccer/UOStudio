a6f36e0023b8cb30b557d9aa81b52dcfd4bc20d6
deccer <decccer@outlook.com>
2020-11-14 16:32:29 +0400

    Fix language switcher

---
+++ src/UOStudio.Client.Engine/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace UOStudio.Client.Engine
+{
+    public static class Constants
+    {
+        public static class Language
+        {
+            public const string English = nameof(English);
+            public const string German = nameof(German);
+        }
+    }
+}
--- src/UOStudio.Client.Engine/Windows/General/SettingsWindow.cs
+++ src/UOStudio.Client.Engine/Windows/General/SettingsWindow.cs
@@ -1,4 +1,6 @@
-﻿using System.Globalization;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Threading;
 using ImGuiNET;
 using UOStudio.Client.Core.Settings;
@@ -10,26 +12,58 @@ namespace UOStudio.Client.Engine.Windows.General
     {
         private readonly IAppSettingsProvider _appSettingsProvider;
 
+        private readonly IDictionary<string, CultureInfo> _cultures;
+
         public SettingsWindow(IAppSettingsProvider appSettingsProvider)
             : base(ResGeneral.WindowCaptionSettings)
         {
+            _cultures = new Dictionary<string, CultureInfo>
+            {
+                { Constants.Language.English, new CultureInfo("en-US") },
+                { Constants.Language.German, new CultureInfo("de-DE") }
+            };
             _appSettingsProvider = appSettingsProvider;
         }
 
         protected override void DrawInternal()
         {
-            if (ImGui.Button("en"))
+            if (ImGui.BeginTabBar("Settings"))
             {
-                ResGeneral.Culture = new CultureInfo("en-US");
-                Thread.CurrentThread.CurrentUICulture = ResGeneral.Culture = Thread.CurrentThread.CurrentCulture;
+                if (ImGui.BeginTabItem("General"))
+                {
+                    var uoPath = _appSettingsProvider.AppSettings.General.UltimaOnlineBasePath;
+                    ImGui.TextUnformatted("Path to UO");
+                    ImGui.SameLine();
+                    if (ImGui.InputText("##pathbox", ref uoPath, 240) && Directory.Exists(uoPath))
+                    {
+                        _appSettingsProvider.AppSettings.General.UltimaOnlineBasePath = uoPath;
+                    }
+
+                    if (ImGui.Button("English"))
+                    {
+                        SetCulture(Constants.Language.English);
+                    }
+
+                    if (ImGui.Button("German"))
+                    {
+                        SetCulture(Constants.Language.German);
+                    }
+
+                    ImGui.EndTabItem();
+                }
+
+                ImGui.EndTabBar();
             }
 
-            if (ImGui.Button("de"))
-            {
-                ResGeneral.Culture = new CultureInfo("de-DE");
-                Thread.CurrentThread.CurrentUICulture = ResGeneral.Culture = Thread.CurrentThread.CurrentCulture;
-            }
             ImGui.End();
+        }
+
+        private void SetCulture(string language)
+        {
+            ResGeneral.Culture = _cultures.TryGetValue(language, out var culture)
+                ? culture
+                : CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentUICulture = ResGeneral.Culture;
         }
     }
 }
