2be10225a27ed7ace97ffba82d288f193e16357c
deccer <decccer@outlook.com>
2020-11-26 02:05:38 +0400

    Add EF.Core dependencies

---
+++ src/UOStudio.Server.Data/UOStudioDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UOStudio.Server.Data
+{
+    public class UOStudioDbContext : DbContext
+    {
+        public UOStudioDbContext(DbContextOptions<UOStudioDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; }
+
+        public DbSet<Project> Projects { get; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .Property(account => account.Version)
+                .IsRowVersion();
+            modelBuilder.Entity<Project>()
+                .Property(project => project.Version)
+                .IsRowVersion();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
