db46d0bf300d22ea922e459c7921fde65399ddb7
deccer <decccer@outlook.com>
2020-11-30 12:43:43 +0400

    Relocate Windows

---
+++ src/UOStudio.Client.Engine/Windows/General/ChatWindow.cs
@@ -0,0 +1,14 @@
+﻿namespace UOStudio.Client.Engine.Windows.General
+{
+    public sealed class ChatWindow : Window
+    {
+        public ChatWindow()
+            : base("Chat")
+        {
+        }
+
+        protected override void DrawInternal()
+        {
+        }
+    }
+}
---
+++ src/UOStudio.Client.Engine/Windows/General/StyleEditorWindow.cs
@@ -0,0 +1,19 @@
+﻿using ImGuiNET;
+
+namespace UOStudio.Client.Engine.Windows.General
+{
+    public sealed class StyleEditorWindow : Window
+    {
+        public StyleEditorWindow()
+            : base("Edit Style")
+        {
+        }
+
+        protected override void DrawInternal()
+        {
+            var currentStyle = ImGui.GetStyle();
+            ImGui.ShowStyleEditor(currentStyle);
+            ImGui.End();
+        }
+    }
+}
