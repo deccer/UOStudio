3b044a8dcca22b91253cee9abf558975d698da03
deccer <decccer@outlook.com>
2020-12-26 17:33:46 +0400

    Add client side account

---
+++ src/UOStudio.Client.Core/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UOStudio.Core;
+
+namespace UOStudio.Client.Core
+{
+    public sealed class Account
+    {
+        public Account(Guid id, string name, Permissions permissions)
+        {
+            Id = id;
+            Name = name;
+            Permissions = permissions;
+        }
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Permissions Permissions { get; set; }
+    }
+}
