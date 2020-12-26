19d3096c63c360d018b08538155e99c81c0aa33b
deccer <decccer@outlook.com>
2020-12-26 17:34:47 +0400

    Fix formatting

--- src/UOStudio.Client.Engine/IO/MulFileIndex.cs
+++ src/UOStudio.Client.Engine/IO/MulFileIndex.cs
@@ -23,35 +23,33 @@ namespace UOStudio.Client.Engine.IO
 
             var length = (int)(_fileInfo.Length / 3 / 4);
 
-            using (var index = new FileStream(_indexPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using var index = new FileStream(_indexPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            var binaryReader = new BinaryReader(index);
+
+            var count = (int)(index.Length / 12);
+
+            for (var i = 0; i < count && i < length; ++i)
             {
-                var binaryReader = new BinaryReader(index);
-
-                var count = (int)(index.Length / 12);
-
-                for (var i = 0; i < count && i < length; ++i)
+                var entry = new FileIndexEntry
                 {
-                    var entry = new FileIndexEntry
-                    {
-                        Lookup = binaryReader.ReadInt32(),
-                        Length = binaryReader.ReadInt32(),
-                        Extra = binaryReader.ReadInt32()
-                    };
+                    Lookup = binaryReader.ReadInt32(),
+                    Length = binaryReader.ReadInt32(),
+                    Extra = binaryReader.ReadInt32()
+                };
 
-                    entries.Add(entry);
-                }
+                entries.Add(entry);
+            }
 
-                for (var i = count; i < length; ++i)
+            for (var i = count; i < length; ++i)
+            {
+                var entry = new FileIndexEntry
                 {
-                    var entry = new FileIndexEntry
-                    {
-                        Lookup = -1,
-                        Length = -1,
-                        Extra = -1
-                    };
+                    Lookup = -1,
+                    Length = -1,
+                    Extra = -1
+                };
 
-                    entries.Add(entry);
-                }
+                entries.Add(entry);
             }
 
             return entries.ToArray();
