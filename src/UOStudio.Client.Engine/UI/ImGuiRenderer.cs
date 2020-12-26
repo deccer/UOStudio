f4ed7e680d3138568e63ea0e602033e87618db84
deccer <decccer@outlook.com>
2020-12-26 17:35:49 +0400

    Imgui dispose font atlas

--- src/UOStudio.Client.Engine/UI/ImGuiRenderer.cs
+++ src/UOStudio.Client.Engine/UI/ImGuiRenderer.cs
@@ -9,7 +9,7 @@ using Num = System.Numerics;
 
 namespace UOStudio.Client.Engine.UI
 {
-    public sealed class ImGuiRenderer
+    public sealed class ImGuiRenderer : IDisposable
     {
         private readonly GraphicsDevice _graphicsDevice;
 
@@ -26,8 +26,9 @@ namespace UOStudio.Client.Engine.UI
 
         private readonly Dictionary<IntPtr, Texture2D> _loadedTextures;
 
+        private Texture2D _fontAtlasTexture;
         private int _textureId;
-        private IntPtr? _fontTextureId;
+        private IntPtr? _fontAtlasTextureId;
         private int _scrollWheelValue;
 
         private readonly List<int> _keys = new List<int>();
@@ -51,7 +52,10 @@ namespace UOStudio.Client.Engine.UI
                 SlopeScaleDepthBias = 0
             };
 
-            SetupInput();
+            var io = ImGui.GetIO();
+            io.ConfigFlags = ImGuiConfigFlags.DockingEnable;
+            SetupInput(io);
+            //SetRedStyle(ImGui.GetStyle());
         }
 
         public unsafe void RebuildFontAtlas()
@@ -59,25 +63,20 @@ namespace UOStudio.Client.Engine.UI
             var io = ImGui.GetIO();
             io.Fonts.GetTexDataAsRGBA32(out byte* pixelData, out var width, out var height, out var bytesPerPixel);
 
-            // Copy the data to a managed array
             var pixels = new byte[width * height * bytesPerPixel];
             Marshal.Copy(new IntPtr(pixelData), pixels, 0, pixels.Length);
 
-            // Create and register the texture as an XNA texture
-            var tex2d = new Texture2D(_graphicsDevice, width, height, false, SurfaceFormat.Color);
-            tex2d.SetData(pixels);
+            _fontAtlasTexture?.Dispose();
+            _fontAtlasTexture = new Texture2D(_graphicsDevice, width, height, false, SurfaceFormat.Color);
+            _fontAtlasTexture.SetData(pixels);
 
-            // Should a texture already have been build previously, unbind it first so it can be deallocated
-            if (_fontTextureId.HasValue)
+            if (_fontAtlasTextureId.HasValue)
             {
-                UnbindTexture(_fontTextureId.Value);
+                UnbindTexture(_fontAtlasTextureId.Value);
             }
 
-            // Bind the new texture to an ImGui-friendly id
-            _fontTextureId = BindTexture(tex2d);
-
-            // Let ImGui know where to find the texture
-            io.Fonts.SetTexID(_fontTextureId.Value);
+            _fontAtlasTextureId = BindTexture(_fontAtlasTexture);
+            io.Fonts.SetTexID(_fontAtlasTextureId.Value);
             io.Fonts.ClearTexData();
         }
 
@@ -88,6 +87,11 @@ namespace UOStudio.Client.Engine.UI
             _loadedTextures.Add(id, texture);
 
             return id;
+        }
+
+        public void Dispose()
+        {
+            _fontAtlasTexture?.Dispose();
         }
 
         public void UnbindTexture(IntPtr textureId)
@@ -111,10 +115,8 @@ namespace UOStudio.Client.Engine.UI
             RenderDrawData(ImGui.GetDrawData());
         }
 
-        protected void SetupInput()
+        protected void SetupInput(ImGuiIOPtr io)
         {
-            var io = ImGui.GetIO();
-
             _keys.Add(io.KeyMap[(int)ImGuiKey.Tab] = (int)Keys.Tab);
             _keys.Add(io.KeyMap[(int)ImGuiKey.LeftArrow] = (int)Keys.Left);
             _keys.Add(io.KeyMap[(int)ImGuiKey.RightArrow] = (int)Keys.Right);
@@ -535,9 +537,59 @@ namespace UOStudio.Client.Engine.UI
             style.Colors[(int)ImGuiCol.ModalWindowDimBg] = new Num.Vector4(0.80f, 0.80f, 0.80f, 0.35f);
         }
 
-        public static void EnableDocking()
+        private static void SetXStyle(ImGuiStylePtr style)
         {
-            ImGui.GetIO().ConfigFlags = ImGuiConfigFlags.DockingEnable;
+            var colors = ImGui.GetStyle().Colors;
+            colors[(int)ImGuiCol.Text] = new Num.Vector4(0.92f, 0.63f, 0.13f, 1.00f);
+            colors[(int)ImGuiCol.TextDisabled] = new Num.Vector4(0.39f, 0.29f, 0.00f, 1.00f);
+            colors[(int)ImGuiCol.WindowBg] = new Num.Vector4(0.00f, 0.00f, 0.00f, 1.00f);
+            colors[(int)ImGuiCol.ChildBg] = new Num.Vector4(0.10f, 0.11f, 0.12f, 1.00f);
+            colors[(int)ImGuiCol.PopupBg] = new Num.Vector4(0.21f, 0.22f, 0.25f, 0.90f);
+            colors[(int)ImGuiCol.Border] = new Num.Vector4(0.43f, 0.43f, 0.50f, 0.50f);
+            colors[(int)ImGuiCol.BorderShadow] = new Num.Vector4(0.00f, 0.00f, 0.00f, 0.00f);
+            colors[(int)ImGuiCol.FrameBg] = new Num.Vector4(0.04f, 0.04f, 0.04f, 1.00f);
+            colors[(int)ImGuiCol.FrameBgHovered] = new Num.Vector4(0.98f, 0.59f, 0.01f, 0.40f);
+            colors[(int)ImGuiCol.FrameBgActive] = new Num.Vector4(0.89f, 0.64f, 0.02f, 0.67f);
+            colors[(int)ImGuiCol.TitleBg] = new Num.Vector4(0.27f, 0.27f, 0.27f, 1.00f);
+            colors[(int)ImGuiCol.TitleBgActive] = new Num.Vector4(0.22f, 0.22f, 0.22f, 1.00f);
+            colors[(int)ImGuiCol.TitleBgCollapsed] = new Num.Vector4(0.20f, 0.20f, 0.30f, 0.75f);
+            colors[(int)ImGuiCol.MenuBarBg] = new Num.Vector4(0.25f, 0.25f, 0.25f, 0.47f);
+            colors[(int)ImGuiCol.ScrollbarBg] = new Num.Vector4(0.21f, 0.22f, 0.25f, 1.00f);
+            colors[(int)ImGuiCol.ScrollbarGrab] = new Num.Vector4(0.31f, 0.31f, 0.31f, 1.00f);
+            colors[(int)ImGuiCol.ScrollbarGrabHovered] = new Num.Vector4(0.41f, 0.41f, 0.41f, 1.00f);
+            colors[(int)ImGuiCol.ScrollbarGrabActive] = new Num.Vector4(0.51f, 0.51f, 0.51f, 1.00f);
+            colors[(int)ImGuiCol.CheckMark] = new Num.Vector4(1.00f, 0.75f, 0.00f, 1.00f);
+            colors[(int)ImGuiCol.SliderGrab] = new Num.Vector4(1.00f, 0.67f, 0.00f, 1.00f);
+            colors[(int)ImGuiCol.SliderGrabActive] = new Num.Vector4(0.94f, 0.36f, 0.07f, 1.00f);
+            colors[(int)ImGuiCol.Button] = new Num.Vector4(0.53f, 0.53f, 0.53f, 0.40f);
+            colors[(int)ImGuiCol.ButtonHovered] = new Num.Vector4(0.44f, 0.48f, 0.44f, 1.00f);
+            colors[(int)ImGuiCol.ButtonActive] = new Num.Vector4(0.85f, 0.85f, 0.85f, 1.00f);
+            colors[(int)ImGuiCol.Header] = new Num.Vector4(0.81f, 0.55f, 0.03f, 0.31f);
+            colors[(int)ImGuiCol.HeaderHovered] = new Num.Vector4(0.89f, 0.45f, 0.03f, 0.80f);
+            colors[(int)ImGuiCol.HeaderActive] = new Num.Vector4(0.92f, 0.33f, 0.03f, 1.00f);
+            colors[(int)ImGuiCol.Separator] = new Num.Vector4(0.43f, 0.43f, 0.50f, 0.50f);
+            colors[(int)ImGuiCol.SeparatorHovered] = new Num.Vector4(0.93f, 0.29f, 0.02f, 0.78f);
+            colors[(int)ImGuiCol.SeparatorActive] = new Num.Vector4(0.90f, 0.21f, 0.05f, 1.00f);
+            colors[(int)ImGuiCol.ResizeGrip] = new Num.Vector4(1.00f, 0.82f, 0.00f, 0.25f);
+            colors[(int)ImGuiCol.ResizeGripHovered] = new Num.Vector4(0.93f, 0.31f, 0.05f, 0.67f);
+            colors[(int)ImGuiCol.ResizeGripActive] = new Num.Vector4(0.93f, 0.38f, 0.06f, 0.95f);
+            colors[(int)ImGuiCol.Tab] = new Num.Vector4(0.27f, 0.27f, 0.27f, 0.86f);
+            colors[(int)ImGuiCol.TabHovered] = new Num.Vector4(0.52f, 0.52f, 0.52f, 0.80f);
+            colors[(int)ImGuiCol.TabActive] = new Num.Vector4(0.49f, 0.49f, 0.49f, 0.90f);
+            colors[(int)ImGuiCol.TabUnfocused] = new Num.Vector4(0.22f, 0.22f, 0.22f, 0.97f);
+            colors[(int)ImGuiCol.TabUnfocusedActive] = new Num.Vector4(0.16f, 0.16f, 0.16f, 1.00f);
+            colors[(int)ImGuiCol.DockingPreview] = new Num.Vector4(0.88f, 0.14f, 0.01f, 0.70f);
+            colors[(int)ImGuiCol.DockingEmptyBg] = new Num.Vector4(0.14f, 0.14f, 0.14f, 1.00f);
+            colors[(int)ImGuiCol.PlotLines] = new Num.Vector4(0.61f, 0.61f, 0.61f, 1.00f);
+            colors[(int)ImGuiCol.PlotLinesHovered] = new Num.Vector4(1.00f, 0.43f, 0.35f, 1.00f);
+            colors[(int)ImGuiCol.PlotHistogram] = new Num.Vector4(0.90f, 0.70f, 0.00f, 1.00f);
+            colors[(int)ImGuiCol.PlotHistogramHovered] = new Num.Vector4(1.00f, 0.60f, 0.00f, 1.00f);
+            colors[(int)ImGuiCol.TextSelectedBg] = new Num.Vector4(0.80f, 0.48f, 0.02f, 0.35f);
+            colors[(int)ImGuiCol.DragDropTarget] = new Num.Vector4(1.00f, 1.00f, 0.00f, 0.90f);
+            colors[(int)ImGuiCol.NavHighlight] = new Num.Vector4(0.90f, 0.68f, 0.04f, 1.00f);
+            colors[(int)ImGuiCol.NavWindowingHighlight] = new Num.Vector4(1.00f, 1.00f, 1.00f, 0.70f);
+            colors[(int)ImGuiCol.NavWindowingDimBg] = new Num.Vector4(0.80f, 0.80f, 0.80f, 0.20f);
+            colors[(int)ImGuiCol.ModalWindowDimBg] = new Num.Vector4(0.80f, 0.80f, 0.80f, 0.35f);
         }
 
         public static void SetStyle(UiStyle uiStyle)
@@ -567,7 +619,7 @@ namespace UOStudio.Client.Engine.UI
                     SetCherryStyle(style);
                     break;
                 case UiStyle.Red:
-                    SetRedStyle(style);
+                    SetXStyle(style);
                     break;
             }
         }
