c0111ddcf23ee8bfb22bfe855d14a02b3257772a
deccer <decccer@outlook.com>
2021-03-10 10:43:00 +0400

    NetworkClient can talk to Api

--- src/UOStudio.Client/Services/NetworkClient.cs
+++ src/UOStudio.Client/Services/NetworkClient.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Net.Http.Json;
-using System.Net.Mime;
-using System.Threading;
 using CSharpFunctionalExtensions;
 using LiteNetLib;
 using Microsoft.Extensions.Configuration;
@@ -41,6 +38,9 @@ namespace UOStudio.Client.Services
         public event Action Disconnected;
         public event Action<int, IReadOnlyCollection<ProjectDto>> LoginSucceeded;
         public event Action<string> LoginFailed;
+
+        public event Action<IReadOnlyCollection<ProjectDto>> GetProjectsSucceeded;
+        public event Action<string> GetProjectsFailed;
         public event Action<ProjectDetailDto> GetProjectDetailsSucceeded;
         public event Action<string> GetProjectDetailsFailed;
 
@@ -48,7 +48,7 @@ namespace UOStudio.Client.Services
         {
             _selectedProfile = profile;
 
-            var getProjectsResult = GetProjects();
+            var getProjectsResult = GetProjectsAfterLogin();
             if (getProjectsResult.IsSuccess)
             {
                 var loginSucceeded = LoginSucceeded;
@@ -72,43 +72,106 @@ namespace UOStudio.Client.Services
 
         public void GetProjectDetailsByProjectId(int projectId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var httpClientResult = _httpClientFactory.CreateWithBearerAuthentication(_tokenService, _selectedProfile);
+                if (httpClientResult.IsFailure)
+                {
+                    var getProjectDetailsFailed = GetProjectDetailsFailed;
+                    getProjectDetailsFailed?.Invoke(httpClientResult.Error);
+                }
+
+                var getProjectDetailsResponse = httpClientResult.Value.GetAsync($"{_apiEndpoint}/api/project/{projectId}").GetAwaiter().GetResult();
+                if (getProjectDetailsResponse.IsSuccessStatusCode)
+                {
+                    var getProjectDetailsSucceeded = GetProjectDetailsSucceeded;
+                    getProjectDetailsSucceeded?.Invoke(getProjectDetailsResponse.Content.ReadFromJsonAsync<ProjectDetailDto>().GetAwaiter().GetResult());
+                }
+
+                httpClientResult.Value.Dispose();
+            }
+            catch (Exception exception)
+            {
+                var getProjectDetailsFailed = GetProjectDetailsFailed;
+                getProjectDetailsFailed?.Invoke(exception.Message);
+            }
         }
 
         public void GetProjectDetailsByProjectName(string projectName)
         {
-            throw new NotImplementedException();
-        }
-
-        public Result<IReadOnlyCollection<ProjectDto>> GetProjects()
-        {
             try
             {
-                var retrieveTokenResult = _tokenService
-                    .RetrieveTokenAsync(_selectedProfile.UserName, _selectedProfile.Password)
-                    .GetAwaiter()
-                    .GetResult();
-                if (retrieveTokenResult.IsFailure)
+                var httpClientResult = _httpClientFactory.CreateWithBearerAuthentication(_tokenService, _selectedProfile);
+                if (httpClientResult.IsFailure)
                 {
-                    return Result.Failure<IReadOnlyCollection<ProjectDto>>(retrieveTokenResult.Error);
+                    var getProjectDetailsFailed = GetProjectDetailsFailed;
+                    getProjectDetailsFailed?.Invoke(httpClientResult.Error);
                 }
 
-                using var httpClient = _httpClientFactory.CreateClient();
-                httpClient.DefaultRequestHeaders.Clear();
-                httpClient.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse(MediaTypeNames.Application.Json));
-                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", retrieveTokenResult.Value);
-
-                var getProjectResponse = httpClient.GetAsync($"{_apiEndpoint}/api/project").GetAwaiter().GetResult();
-                if (getProjectResponse.IsSuccessStatusCode)
+                projectName = projectName.Replace(" ", "+");
+                var getProjectDetailsResponse = httpClientResult.Value.GetAsync($"{_apiEndpoint}/api/project/{projectName}").GetAwaiter().GetResult();
+                if (getProjectDetailsResponse.IsSuccessStatusCode)
                 {
-                    return Result.Success<IReadOnlyCollection<ProjectDto>>(getProjectResponse.Content.ReadFromJsonAsync<List<ProjectDto>>().GetAwaiter().GetResult());
+                    var getProjectDetailsSucceeded = GetProjectDetailsSucceeded;
+                    getProjectDetailsSucceeded?.Invoke(getProjectDetailsResponse.Content.ReadFromJsonAsync<ProjectDetailDto>().GetAwaiter().GetResult());
                 }
 
-                throw new Exception();
+                httpClientResult.Value.Dispose();
             }
             catch (Exception exception)
             {
-                return Result.Failure<IReadOnlyCollection<ProjectDto>>(exception.ToString());
+                var getProjectDetailsFailed = GetProjectDetailsFailed;
+                getProjectDetailsFailed?.Invoke(exception.Message);
+            }
+        }
+
+        public void GetProjects()
+        {
+            try
+            {
+                var httpClientResult = _httpClientFactory.CreateWithBearerAuthentication(_tokenService, _selectedProfile);
+                if (httpClientResult.IsFailure)
+                {
+                    var getProjectsFailed = GetProjectsFailed;
+                    getProjectsFailed?.Invoke(httpClientResult.Error);
+                }
+
+                var getProjectResponse = httpClientResult.Value.GetAsync($"{_apiEndpoint}/api/project").GetAwaiter().GetResult();
+                if (getProjectResponse.IsSuccessStatusCode)
+                {
+                    var getProjectsSucceeded = GetProjectsSucceeded;
+                    getProjectsSucceeded?.Invoke(getProjectResponse.Content.ReadFromJsonAsync<List<ProjectDto>>().GetAwaiter().GetResult());
+                }
+
+                httpClientResult.Value.Dispose();
+            }
+            catch (Exception exception)
+            {
+                var getProjectsFailed = GetProjectsFailed;
+                getProjectsFailed?.Invoke(exception.Message);
+            }
+        }
+
+        private Result<IReadOnlyCollection<ProjectDto>> GetProjectsAfterLogin()
+        {
+            try
+            {
+                var httpClientResult = _httpClientFactory.CreateWithBearerAuthentication(_tokenService, _selectedProfile);
+                if (httpClientResult.IsFailure)
+                {
+                    return Result.Failure<IReadOnlyCollection<ProjectDto>>(httpClientResult.Error);
+                }
+
+                var getProjectResponse = httpClientResult.Value.GetAsync($"{_apiEndpoint}/api/project").GetAwaiter().GetResult();
+                httpClientResult.Value.Dispose();
+
+                return getProjectResponse.IsSuccessStatusCode
+                    ? Result.Success<IReadOnlyCollection<ProjectDto>>(getProjectResponse.Content.ReadFromJsonAsync<List<ProjectDto>>().GetAwaiter().GetResult())
+                    : Result.Failure<IReadOnlyCollection<ProjectDto>>(getProjectResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+            }
+            catch (Exception exception)
+            {
+                return Result.Failure<IReadOnlyCollection<ProjectDto>>(exception.Message);
             }
         }
     }
