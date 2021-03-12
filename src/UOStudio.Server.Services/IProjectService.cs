a47db7fcea219d7fd1fc099614951c82584b10ff
deccer <decccer@outlook.com>
2021-03-12 10:47:07 +0400

    Add project service

---
+++ src/UOStudio.Server.Services/IProjectService.cs
@@ -0,0 +1,9 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace UOStudio.Server.Services
+{
+    public interface IProjectService
+    {
+        Result<bool> CreateProject(string projectTemplatePath, string name);
+    }
+}
---
+++ src/UOStudio.Server.Services/ProjectService.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using CSharpFunctionalExtensions;
+using Serilog;
+using UOStudio.Server.Common;
+
+namespace UOStudio.Server.Services
+{
+    public sealed class ProjectService : IProjectService
+    {
+        private readonly ILogger _logger;
+        private readonly ServerSettings _serverSettings;
+
+        public ProjectService(ILogger logger, ServerSettings serverSettings)
+        {
+            _logger = logger;
+            _serverSettings = serverSettings;
+        }
+
+        public Result<bool> CreateProject(string projectTemplatePath, string name)
+        {
+            var targetProjectPath = Path.Combine(_serverSettings.ProjectsDirectory, name);
+            Directory.CreateDirectory(targetProjectPath);
+
+            var projectTemplateFiles = Directory.GetFiles(projectTemplatePath);
+            foreach (var projectTemplateFile in projectTemplateFiles)
+            {
+                File.Copy(projectTemplateFile, Path.Combine(targetProjectPath, Path.GetFileName(projectTemplateFile)));
+            }
+
+            return Result.Success(true);
+        }
+    }
+}
