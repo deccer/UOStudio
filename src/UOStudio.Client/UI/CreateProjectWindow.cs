14c15afff34026d7dba1c226f7f5a0778d6ed6a2
deccer <decccer@outlook.com>
2021-03-12 10:53:03 +0400

    Add create project window

---
+++ src/UOStudio.Client/UI/CreateProjectWindow.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+
+namespace UOStudio.Client.UI
+{
+    [UsedImplicitly]
+    public sealed class CreateProjectWindow : Window
+    {
+        public CreateProjectWindow(IWindowProvider windowProvider)
+            : base(windowProvider, "Create Project")
+        {
+        }
+
+        protected override void InternalDraw()
+        {
+        }
+    }
+}
