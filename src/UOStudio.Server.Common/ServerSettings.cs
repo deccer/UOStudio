6de62cc31513771a8711c85746e10e0a70f0cb26
deccer <decccer@outlook.com>
2021-05-16 04:20:50 +0400

    Extend ServerSettings

--- src/UOStudio.Server.Common/ServerSettings.cs
+++ src/UOStudio.Server.Common/ServerSettings.cs
@@ -6,12 +6,16 @@
 
         public int Port { get; set; }
 
-        public string DatabaseLocation { get; set; }
+        public string CertificatesDirectory { get; set; }
+
+        public string DatabaseDirectory { get; set; }
 
         public string ProjectsDirectory { get; set; }
 
         public string TemplatesDirectory { get; set; }
 
         public string TemplatesGitRepository { get; set; }
+
+        public string DownloadsDirectory { get; set; }
     }
 }
