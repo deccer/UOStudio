cc3de587644e8062a59705558d537e8d0c220d81
deccer <decccer@outlook.com>
2021-05-16 04:23:53 +0400

    Implement download controller basics

---
+++ src/UOStudio.Server.Api/Controllers/DownloadController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using UOStudio.Server.Common;
+
+namespace UOStudio.Server.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class DownloadController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly ServerSettings _serverSettings;
+
+        public DownloadController(
+            ILogger logger,
+            ServerSettings serverSettings)
+        {
+            _logger = logger.ForContext<DownloadController>();
+            _serverSettings = serverSettings;
+        }
+
+        [HttpGet("{downloadId:guid}", Name = nameof(GetDownload))]
+        public async Task<IActionResult> GetDownload(Guid downloadId)
+        {
+            var downloadFilePath = string.IsNullOrEmpty(Path.GetDirectoryName(_serverSettings.DownloadsDirectory))
+                ? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _serverSettings.DownloadsDirectory, $"{downloadId.ToString()}.7z")
+                : Path.Combine(_serverSettings.DownloadsDirectory, $"{downloadId.ToString()}.7z");
+
+            var downloadStream = System.IO.File.OpenRead(downloadFilePath);
+            return File(downloadStream, MediaTypeNames.Application.Zip);
+        }
+    }
+}
