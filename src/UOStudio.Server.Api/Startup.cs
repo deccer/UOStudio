db13a4c154b09c2c1f090fa506ff09ab404d692f
deccer <decccer@outlook.com>
2021-05-16 04:26:37 +0400

    Cleanup Startup

--- src/UOStudio.Server.Api/Startup.cs
+++ src/UOStudio.Server.Api/Startup.cs
@@ -1,23 +1,22 @@
 using System;
 using System.IO;
-using System.Text;
-using System.Threading.Tasks;
 using MediatR;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Serilog;
 using UOStudio.Common.Core;
+using UOStudio.Server.Api.Extensions;
+using UOStudio.Server.Api.HostedServices;
 using UOStudio.Server.Api.Services;
 using UOStudio.Server.Common;
 using UOStudio.Server.Data;
 using UOStudio.Server.Domain.GetProjectDetailsByName;
+using UOStudio.Server.Services;
 
 namespace UOStudio.Server.Api
 {
@@ -39,67 +38,59 @@ namespace UOStudio.Server.Api
                 .Enrich.WithMemoryUsage()
                 .Enrich.WithCorrelationIdHeader()
                 .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
-                .WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "server.log"))
+                .WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs", "server.log"), rollingInterval: RollingInterval.Day)
                 .CreateLogger();
-
-            void ConfigureOptions(JwtBearerOptions jwtBearerOptions, string audience)
-            {
-                jwtBearerOptions.RequireHttpsMetadata = false;
-                jwtBearerOptions.SaveToken = true;
-                jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["JwtToken:SecretKey"])),
-                    ValidIssuer = _configuration["JwtToken:Issuer"],
-                    ValidAudience = _configuration["JwtToken:Audience"],
-                    ValidateLifetime = true,
-                    ClockSkew = TimeSpan.FromMinutes(1)
-                };
-                if (audience == "access")
-                {
-                    jwtBearerOptions.Events = new JwtBearerEvents
-                    {
-                        OnAuthenticationFailed = context =>
-                        {
-                            if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
-                            {
-                                context.Response.Headers.Add("Token-Expired", "true");
-                            }
-
-                            return Task.CompletedTask;
-                        }
-                    };
-                }
-            }
-
-            services.AddAuthentication(
-                    authOptions =>
-                    {
-                        authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                        authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                    }
-                )
-                .AddJwtBearer(bearerOptions => ConfigureOptions(bearerOptions, "access"))
-                .AddJwtBearer("Refresh", bearerOptions => ConfigureOptions(bearerOptions, "refresh"));
 
             var serverSettings = new ServerSettings();
             _configuration.GetSection("Server").Bind(serverSettings);
+
+            var sevenZipOptions = new SevenZipOptions();
+            _configuration.GetSection("SevenZip").Bind(sevenZipOptions);
+
+            services.AddAsymmetricAuthentication(_configuration);
 
             services.AddSingleton(_configuration);
             services.AddSingleton(Log.Logger);
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "UOStudio.Server.Api", Version = "v1" }); });
             services.AddMediatR(typeof(GetProjectDetailsByNameQueryHandler).Assembly);
-            services.AddDbContextFactory<UOStudioContext>(builder => builder.UseSqlite($"Data Source={Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "uostudio.db")}"));
+            services.AddDbContextFactory<UOStudioContext>((provider, builder) =>
+                {
+                    var ss = provider.GetRequiredService<ServerSettings>();
+                    builder.UseSqlite(string.IsNullOrEmpty(Path.GetDirectoryName(ss.DatabaseDirectory))
+                        ? $"Data Source={Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Database", ss.DatabaseDirectory)}"
+                        : $"Data Source={ss.DatabaseDirectory}");
+                }
+            );
             services.AddSingleton(serverSettings);
+            services.AddSingleton(sevenZipOptions);
             services.AddSingleton<IPasswordHasher, PasswordHasher>();
             services.AddSingleton<IPasswordVerifier, PasswordVerifier>();
+            services.AddSingleton<IAuthenticationService, AuthenticationService>();
+            services.AddSingleton<IUserRepository, UserRepository>();
             services.AddSingleton<ITokenService, TokenService>();
             services.AddSingleton<IUserService, UserService>();
+
+            services.AddSingleton<IProjectService, ProjectService>();
+            services.AddSingleton<IProjectTemplateService, ProjectTemplateService>();
+            services.AddSingleton<ISevenZipService, SevenZipService>();
+            services.AddSingleton<ICommandRunner, CommandRunner>();
+
+            services.AddSingleton<INetworkServer, NetworkServer>();
+            services.AddSingleton<IBackgroundTaskQueue, BackgroundTaskQueue>();
+            services.AddHostedService<MapEditService>();
+            services.AddHostedService<QueuedHostedService>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var contextFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<UOStudioContext>>();
+                using var context = contextFactory.CreateDbContext();
+                context.Database.Migrate();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseSerilogRequestLogging();
@@ -109,11 +100,9 @@ namespace UOStudio.Server.Api
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
