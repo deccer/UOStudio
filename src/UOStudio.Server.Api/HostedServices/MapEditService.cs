e6768920f053f867ea96f79ce3d98eec91b08cdb
deccer <decccer@outlook.com>
2021-05-16 04:25:39 +0400

    Map editing network server will be hosted inside uostudio.server.api

---
+++ src/UOStudio.Server.Api/HostedServices/MapEditService.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using UOStudio.Server.Common;
+
+namespace UOStudio.Server.Api.HostedServices
+{
+    public sealed class MapEditService : IHostedService
+    {
+        private readonly ILogger _logger;
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+        private readonly INetworkServer _networkServer;
+        private readonly ServerSettings _serverSettings;
+
+        public MapEditService(
+            ILogger logger,
+            IHostApplicationLifetime hostApplicationLifetime,
+            INetworkServer networkServer,
+            ServerSettings serverSettings)
+        {
+            _hostApplicationLifetime = hostApplicationLifetime;
+            _networkServer = networkServer;
+            _serverSettings = serverSettings;
+            _logger = logger.ForContext<MapEditService>();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.Debug("MapEditService Starting...");
+            _hostApplicationLifetime.ApplicationStarted.Register(OnApplicationStarted);
+            _hostApplicationLifetime.ApplicationStopping.Register(OnApplicationStopping);
+            _logger.Debug("MapEditService Starting...Done");
+
+            return Task.CompletedTask;
+        }
+
+        private void OnApplicationStarted()
+        {
+            _networkServer.Start(_serverSettings.Port);
+        }
+
+        private void OnApplicationStopping()
+        {
+            _networkServer.Stop();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/UOStudio.Server.Api/HostedServices/QueuedHostedService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using UOStudio.Server.Common;
+
+namespace UOStudio.Server.Api.HostedServices
+{
+    public sealed class QueuedHostedService : BackgroundService
+    {
+        private readonly ILogger _logger;
+
+        public QueuedHostedService(IBackgroundTaskQueue taskQueue, ILogger logger)
+        {
+            TaskQueue = taskQueue;
+            _logger = logger.ForContext<QueuedHostedService>();
+        }
+
+        public IBackgroundTaskQueue TaskQueue { get; }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.Information("QueuedHostedService - Starting");
+
+            await BackgroundProcessing(stoppingToken);
+        }
+
+        private async Task BackgroundProcessing(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var workItem = await TaskQueue.DequeueAsync(stoppingToken);
+
+                try
+                {
+                    await workItem(stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    _logger.Error(ex, "Error occurred executing {@WorkItem}", nameof(workItem));
+                }
+            }
+        }
+
+        public override async Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.Information("QueuedHostedService - Stopping");
+
+            await base.StopAsync(stoppingToken);
+        }
+    }
+}
