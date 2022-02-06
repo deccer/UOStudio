be38ae25af25382d1ec98075172476d8fe9dc976
deccer <decccer@outlook.com>
2022-02-06 16:32:17 +0100

    Implement network serialization for ChunkData

--- src/UOStudio.Common.Network/ChunkData.cs
+++ src/UOStudio.Common.Network/ChunkData.cs
@@ -3,9 +3,9 @@ using UOStudio.Client.Engine.Mathematics;
 
 namespace UOStudio.Common.Network
 {
-    public readonly struct ChunkData : INetSerializable
+    public struct ChunkData : INetSerializable
     {
-        public const int ChunkSize = 16;
+        public const int ChunkSize = 8;
 
         public ChunkData(
             int worldId,
@@ -21,7 +21,7 @@ namespace UOStudio.Common.Network
 
         public int WorldId { get; }
 
-        public Point Position { get; }
+        public Point Position { get; private set; }
 
         public ChunkStaticTileData[] StaticTileData { get; }
 
@@ -29,14 +29,36 @@ namespace UOStudio.Common.Network
 
         public void Deserialize(NetDataReader reader)
         {
+            var worldId = reader.GetInt();
+            var chunkSize = reader.GetInt();
+            var positionX = reader.GetInt();
+            var positionY = reader.GetInt();
+            Position = new Point(positionX, positionY);
+            var chunkLength = ChunkData.ChunkSize * ChunkData.ChunkSize;
+            for (var i = 0; i < chunkLength; i++)
+            {
+                StaticTileData[i] = new ChunkStaticTileData(
+                    reader.GetUShort(),
+                    reader.GetInt(),
+                    reader.GetInt());
+            }
+
+            for (var i = 0; i < chunkLength; i++)
+            {
+                ItemTileData[i] = new ChunkItemTileData(
+                    reader.GetUShort(),
+                    reader.GetInt(),
+                    reader.GetInt());
+            }
         }
 
         public void Serialize(NetDataWriter writer)
         {
             writer.Put(WorldId);
+            writer.Put(ChunkData.ChunkSize);
+
             writer.Put(Position.X);
             writer.Put(Position.Y);
-            writer.Put(ChunkData.ChunkSize);
             var chunkLength = ChunkData.ChunkSize * ChunkData.ChunkSize;
             for (int i = 0; i < chunkLength; i++)
             {
